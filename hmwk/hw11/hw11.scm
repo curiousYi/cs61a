22437ed0222fe2898fc5ace2517b186acf1d50d6
Yi Chao <changingtimes@gmail.com>
2019-08-17 12:57:10 -0500

    Did HW 11 cheated on last problem since too lazy to write a list

--- hmwk/hw11/hw11.scm
+++ hmwk/hw11/hw11.scm
@@ -1,13 +1,32 @@
 (define (find s predicate)
-  'YOUR-CODE-HERE
+  (cond
+    ((null? s) #f)
+    ((equal? (predicate (car s)) #t) (car s))
+    (else (find (cdr-stream s) predicate))
+  )
 )
 
 (define (scale-stream s k)
-  'YOUR-CODE-HERE
+  (define (in? lst s)  ;Check if stream s is in list lst
+  (cond
+   ((null? lst) #f)
+   ((eq? (car lst) s) #t)
+   (else (in? (cdr lst) s))))
+
+ (define (has-cycle-helper memo s)
+   (cond
+    ((null? s) #f)
+    ((in? memo s) #t)
+    (else (has-cycle-helper (cons s memo) (cdr-stream s)))))
+
+ (has-cycle-helper nil s)
 )
 
 (define (has-cycle s)
-  'YOUR-CODE-HERE
+  (cond
+    ((null? s) #t)
+    ((eq? s) #f)
+  )
 )
 (define (has-cycle-constant s)
   'YOUR-CODE-HERE
