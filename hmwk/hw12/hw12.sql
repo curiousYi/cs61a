e5588bc4600ea3275940d93a403911bb7368fc77
Yi Chao <changingtimes@gmail.com>
2019-09-03 00:12:49 -0500

    Working on hmwk 12 problems size of dogs siblings

---
+++ hmwk/hw12/hw12.sql
@@ -0,0 +1,58 @@
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE dogs AS
+  SELECT "abraham" AS name, "long" AS fur, 26 AS height UNION
+  SELECT "barack"         , "short"      , 52           UNION
+  SELECT "clinton"        , "long"       , 47           UNION
+  SELECT "delano"         , "long"       , 46           UNION
+  SELECT "eisenhower"     , "short"      , 35           UNION
+  SELECT "fillmore"       , "curly"      , 32           UNION
+  SELECT "grover"         , "short"      , 28           UNION
+  SELECT "herbert"        , "curly"      , 31;
+
+CREATE TABLE sizes AS
+  SELECT "toy" AS size, 24 AS min, 28 AS max UNION
+  SELECT "mini"       , 28       , 35        UNION
+  SELECT "medium"     , 35       , 45        UNION
+  SELECT "standard"   , 45       , 60;
+
+-------------------------------------------------------------
+-- PLEASE DO NOT CHANGE ANY SQL STATEMENTS ABOVE THIS LINE --
+-------------------------------------------------------------
+
+-- The size of each dog
+CREATE TABLE size_of_dogs AS
+  SELECT 
+    dogs.name, sizes.size
+  FROM dogs, sizes
+  WHERE dogs.height > sizes.min AND dogs.height <= sizes.max;
+
+
+-- All dogs with parents ordered by decreasing height of their parent
+CREATE TABLE by_height AS
+  SELECT parents.child
+  FROM dogs, parents
+  WHERE dogs.name = parents.parent
+  ORDER BY dogs.height DESC;
+
+-- Sentences about siblings that are the same size
+CREATE TABLE sentences AS
+  with same_size (sib1, sib2) as
+    (SELECT 
+      dogs.name, doggo.name
+      FROM dogs, dogs as doggo, parents, parents as doggo_parents, size_of_dogs, size_of_dogs as size_of_doggos
+      WHERE dogs.name = parents.child AND doggo.name = doggo_parents.child AND parents.parent = doggo_parents.parent and dogs.name <> doggo.name
+      AND dogs.name = size_of_dogs.name AND doggo.name = size_of_doggos.name AND size_of_doggos.size = size_of_dogs.size
+    )
+  SELECT * FROM same_size;
+
+-- Ways to stack 4 dogs to a height of at least 170, ordered by total height
+CREATE TABLE stacks AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
